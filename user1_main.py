6ab7ec560ca64331ae85b83f6e89984f41207166
Trần Nhật Trường <baitap29012004@gmail.com>
2025-12-23 22:58:42 +0700

    test 3

--- user1_main.py
+++ user1_main.py
@@ -3,4 +3,4 @@ import numpy as np
 print ("Hello github")
 
 # cái này là ấn vào cái clone responsitory và gán link nhánh main nên mọi sửa chữa trong này là sẽ sửa nhánh main
-# bây giờ sẽ dùng thử máy này để kết nối với nhanh user1 thật
+# bây giờ sẽ dùng thử máy này để kết nối với nhanh user1 thật nhưng chắc phải tạo một folder khác đẻ chứa cái nhanh user 1 đó
